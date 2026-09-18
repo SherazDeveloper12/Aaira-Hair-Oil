c43c91b63df2d9c823a0b883de6d21235f68a599
Sheraz <talhayounus065@gmail.com>
2026-09-19 02:38:47 +0500

    Add About component and enhance Header navigation links

---
+++ app/components/About/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div id='About' className="bg-secondary/70 flex flex-col items-center justify-center gap-4 lg:gap-8 px-4 lg:px-16 m-auto relative py-16  md:py-26">
+      <h2 className="text-5xl font-bold text-primary">About Us</h2>
+      
+      <p className="text-center text-xl italic"> <span className="text-left w-full  text-4xl font-bold italic">" </span> Aaira is a return to the basics: good ingredients, a little consistency, and the confidence that comes from caring for yourself well. Our hair oil nourishes the scalp, strengthens from within, and leaves every strand with a natural, weightless shine. Carefully made with nourishing ingredients, it helps support a healthy scalp, reduce dryness, and keep your hair feeling soft, smooth, and manageable. Whether you are beginning a new self-care ritual or adding a simple step to your existing routine, Aaira makes caring for your hair an easy and relaxing experience. With regular use, enjoy hair that looks healthier, feels stronger, and radiates its natural beauty.
+      <span className="text-right w-full  text-4xl font-bold italic "> "</span>
+      </p>
+
+    </div>
+  )
+}
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,15 +1,26 @@
 'use client'
-import React from 'react'
-import ShoppingCartSignIcon from '@iconify-react/el/shopping-cart-sign';
-import UserSolidIcon from '@iconify-react/mynaui/user-solid';
-import { useAppDataProvider } from '@/app/context/Context';
+import React, { useEffect } from 'react'
+
 import { useDispatch, useSelector } from 'react-redux';
+
+
 import { toggleCart } from '@/app/store/slices/cart';
 import { useRouter } from 'next/dist/client/components/navigation';
 import { Package, ShoppingCart } from 'lucide-react';
+import { fetchStoreDeliveryCharges, fetchStoreDeliveryChargesLocally, fetchStorePaymentMethods, fetchStorePaymentMethodsLocally, setUser } from '@/app/store/slices/auth';
+import { fetchProducts } from '@/app/store/slices/product';
 export default function Header() {
     const router = useRouter();
     const dispatch = useDispatch();
+     
+      useEffect(() => {
+        dispatch(setUser());
+        dispatch(fetchProducts());
+        dispatch(fetchStoreDeliveryChargesLocally());
+        dispatch(fetchStorePaymentMethodsLocally());
+        dispatch(fetchStorePaymentMethods());
+        dispatch(fetchStoreDeliveryCharges());
+      }, []);
   return (
     <header className="flex bg-white h-12 lg:h-24 w-full items-center justify-between shadow-md  px-4 lg:px-8   sticky top-0 z-50">
         <div
@@ -33,10 +44,11 @@ function Navigation() {
     const router = useRouter();
     const navItems = [
         {"label": "Home", "href": "/"},
-        {"label": "Shop", "href": "#product"},
-        {"label": "Our Benefits", "href": "#benefits"},
-        {"label": "About us", "href": "#About-us"},
-        {"label": "Contact", "href": "#contact"},
+        {"label": "Shop", "href": "/shop"},
+        {"label": "Aaira Hair Oil", "href": "/#product"},
+        {"label": "Benefits", "href": "/#benefits"},
+        {"label": "About us", "href": "/#About"},
+       
     ];
   return (
     <div className="flex">
